0e0ff878671912fad00ff140dc0de9b0b534fdf5
Pankaj Patel <86610118+pankaj1980patel@users.noreply.github.com>
2023-04-10 06:22:59 +0000

    modified main

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-from flask import Flask, request, Response
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
-def hello():
-  if request.method == 'POST':
-    message = request.get_json()
-
-    print("i got a meassage")
-
-    print(message, "\n")
-    return Response('OK', status=200)
-  else:
-    return '<h1>hello worls</h1>'
+@app.route('/')
+def hello_world():
+  return 'GreyMatters'
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run()
